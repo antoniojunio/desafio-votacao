89018c493b1d17f624141e9a2830bfb82cdc72ee
antoniojunio <juniordsj@gmail.com>
2024-11-08 09:13:07 -0300

    Implementação do cliente para validação de CPF

---
+++ src/main/java/com/example/votacao/VotacaoSistemaApplication.java
@@ -0,0 +1,12 @@
+package com.example.votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoSistemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotacaoSistemaApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/votacao/exception/CustomException.java
@@ -0,0 +1,7 @@
+package com.example.votacao.exception;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
